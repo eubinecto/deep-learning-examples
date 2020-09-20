c080dec7bddb0d2bb873c52ae73bb48605c6e442
Eu-Bin KIM <tlrndk123@gmail.com>
2020-09-20 22:33:42 +0900

    example code for text classification with huggingface's pipeline.

---
+++ transformers/huggingface/pipeline/text_classification /README.md
@@ -0,0 +1,11 @@
+# `TextClassificaitonPipeline`
+
+## questions
+
+Can I view the probs of all the classes?
+- set the paramter `return_all_scores` to True
+
+
+Can I view the attention weights as well?
+- use `bertviz`
+- https://github.com/jessevig/bertviz
---
+++ transformers/huggingface/pipeline/text_classification /text_classification.py
@@ -0,0 +1,43 @@
+from transformers import pipeline, TextClassificationPipeline, Pipeline
+from typing import cast
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# documentation
+# https://huggingface.co/transformers/main_classes/pipelines.html#transformers.TextClassificationPipeline
+
+# sentences to test
+TEST_SENTENCES = [
+    "I hate you.",  # obviously negative
+    "I love you.",  # obviously positive
+    "Don't beat around the bush!",  # not obviously negative
+    "I sighed in relief to know that the intellectual future of humanity is safe."  # not obviously positive
+]
+
+# his text classification pipeline can currently be loaded from pipeline() using the following task identifier
+TEXT_CLS_ID = 'sentiment-analysis'
+
+
+def main():
+    # hugging face adopts builder pattern for building pipelines.
+    # the default model is bert.
+    # if the model has not been downloaded, it will download the model on the fly
+    logger = logging.getLogger("main")
+    logger.info("loading pipeline...")
+    bert_pipeline: cast(TextClassificationPipeline, Pipeline) = pipeline(TEXT_CLS_ID, return_all_scores=True)
+    # this will execute the __call__ implementation of TextClassificationPipeline
+    # accepts a string, or list of strings as the parameter
+    results = bert_pipeline(TEST_SENTENCES)
+
+    # check out the results
+    for sen, classes in zip(TEST_SENTENCES, results):
+        print("---")
+        # 0: negative, 1: positive
+        classes: list
+        print(sen)
+        print(classes[0]['label'], ":", classes[0]['score'])
+        print(classes[1]['label'], ":", classes[1]['score'])
+
+
+if __name__ == "__main__":
+    main()
